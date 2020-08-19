936e1917ac04cfbca75a8c9f3c7e2c568399056a
Daniel Laguna Soto <daniel.laguna@digib.com>
2020-08-19 23:47:02 +0200

    Update polybar supports multiple monitors
    With the new launch script and polybar configuration. Polybar will
    launch the same bar for all the monitors.

--- local/.local/bin/launch-polybar.sh
+++ local/.local/bin/launch-polybar.sh
@@ -6,7 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar Bar&
+# Launch bars on multiple monitors if available
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload Bar &
+  done
+else
+  polybar --reload Bar &
+fi
 
 echo "Bars launched..."
