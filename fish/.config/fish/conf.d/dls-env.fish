12d59863884192abdb8c66c12a548c388c52fdc6
Daniel Laguna Soto <daniel@sensity.ai>
2021-12-25 22:47:17 +0100

    Add dotenv function

--- fish/.config/fish/conf.d/dls-env.fish
+++ fish/.config/fish/conf.d/dls-env.fish
@@ -1,2 +1,10 @@
+# load variables in ~/.dotenv
+set -l dotenvdir "$HOME/.dotenv"
+if test -e $dotenvdir
+    for envfile in (ls $dotenvdir)
+        type -q dotenv && dotenv $dotenvdir/$envfile
+    end
+end
+
 set -gx EDITOR vim
 set -gx VISUAL emacs
