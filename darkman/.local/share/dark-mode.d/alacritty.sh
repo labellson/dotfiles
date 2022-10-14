6ea8027ec5d345e4fa3e129743529d8d24c47cb0
Daniel Laguna <dani@dani.codes>
2022-10-14 21:26:29 +0200

    add darkman to the config

---
+++ darkman/.local/share/dark-mode.d/alacritty.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+sed -i -e '$s/\*.*$/\*dark/g' ~/.config/alacritty/colorscheme.yml
---
+++ darkman/.local/share/light-mode.d/alacritty.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+sed -i -e '$s/\*.*$/\*light/g' ~/.config/alacritty/colorscheme.yml
