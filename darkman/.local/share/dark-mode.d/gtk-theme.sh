4866318f6982f0553365a6423e9c2f435196be2b
Daniel Laguna <dani@dani.codes>
2022-10-16 18:24:08 +0200

    fix: commands to change gtk-theme does not work
    So I used `sed` in order to change the config file values

--- darkman/.local/share/dark-mode.d/gtk-theme.sh
+++ darkman/.local/share/dark-mode.d/gtk-theme.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
-gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
+# my theme does not change with the following command...
+# gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
+sed -i -e 's/\(gtk-theme-name=\).*$/\1Arc-Dark/g' ~/.config/gtk-3.0/settings.ini
+sed -i -e 's/\(gtk-icon-theme-name=\).*$/\1Papirus-Dark/g' ~/.config/gtk-3.0/settings.ini
