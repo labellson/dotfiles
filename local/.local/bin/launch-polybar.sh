08e87479a82f0297032a287597d54fa9c9cb04e5
Daniel Laguna <dani@dani.codes>
2023-11-05 19:06:04 +0100

    polybar: allow to launch only 1 bar

--- local/.local/bin/launch-polybar.sh
+++ local/.local/bin/launch-polybar.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+# parse cli arguments
+ONE_BAR=false
+while getopts "::o" opt; do
+  case "${opt}" in
+    o) ONE_BAR=true;;
+  esac
+done
+
 # Terminate already running bar instances
 pkill polybar
 
@@ -13,12 +21,15 @@ else
 fi
 
 # Launch bars on multiple monitors if available
+# if [ "${ONE_BAR}" = true && type "xrandr" ]; then
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload "${BAR}" &
-  done
-else
-  polybar --reload "${BAR}" &
+  if [ "${ONE_BAR}" = true ]; then
+    polybar --reload "${BAR}" &
+  else
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      MONITOR=$m polybar --reload "${BAR}" &
+    done
+  fi
 fi
 
 echo "Bars launched..."
