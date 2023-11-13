fc447c1af74f617de3cd250046d15f20de59fec9
Daniel Laguna <dani@dani.codes>
2023-11-13 13:25:45 +0100

    feat(darkman): add solarized light, dark wallpaper

---
+++ darkman/.local/share/light-mode.d/wallpaper.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+BACKGROUND_DST="${HOME}/.background-image"
+BACKGROUND_SRC="${HOME}/.dotfiles/wallpaper/solarized-light.jpg"
+
+ln -sf ${BACKGROUND_SRC} ${BACKGROUND_DST}
+feh --no-fehbg --bg-fill ${BACKGROUND_DST}
