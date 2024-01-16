0fc1d250e1b31c802867fab75de255bd6664f8f5
Daniel Laguna Soto <daniel@sensity.ai>
2024-01-16 13:18:24 +0100

    home: add commons.nix to use in generic servers

---
+++ home-manager/.config/home-manager/commons.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./modules/shell.nix
+    ./fish.nix
+  ];
+
+  # allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    mosh
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/labellson/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself and enable git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+}
