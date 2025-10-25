51e6b68ef0b55b2871e77679c60d49e92cc68a5c
Daniel Laguna <labellson@fastmail.com>
2025-10-25 11:06:24 +0200

    home(nostromo): add swaybg
    Add user unit file to set the wallpaper for niri sessions

--- home-manager/.config/home-manager/nostromo/desktop/wayland.nix
+++ home-manager/.config/home-manager/nostromo/desktop/wayland.nix
@@ -15,4 +15,25 @@
     "niri/config.kdl".source = ../../../../../niri/.config/niri/config.kdl;
     "fuzzel/fuzzel.ini".source = ../../../../../fuzzel/.config/fuzzel/fuzzel.ini;
   };
+
+  home.packages = with pkgs; [
+    swaybg
+  ];
+
+  systemd.user.services.swaybg = {
+    Unit = {
+      Description = "Wallpaper tool for wayland compositor";
+      PartOf = "graphical-session.target";
+      After = "graphical-session.target";
+      Requisite = "graphical-session.target";
+    };
+    Service = {
+      ExecStart = "${pkgs.swaybg}/bin/swaybg -m fill -i %h/.background-image";
+      Restart = "on-failure";
+    };
+    Install = {
+      # TODO: is there a way to do this better??
+      WantedBy = [ "niri.service" ];
+    };
+  };
 }
