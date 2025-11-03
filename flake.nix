371bcf236f3865034a1cc17ef79a8371e7f37da6
Daniel Laguna <labellson@fastmail.com>
2025-11-03 09:38:23 +0100

    nix: remove old flake inputs

--- flake.nix
+++ flake.nix
@@ -7,18 +7,13 @@
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs-25-05.url = "github:nixos/nixpkgs/nixos-25.05";
-    home-manager-25-05 = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-25-05";
-    };
     paisa = {
       url = "github:ananthakumaran/paisa/v0.6.6";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixpkgs-25-05, home-manager-25-05,...}@inputs:
+  outputs = { self, nixpkgs, home-manager, ...}@inputs:
     {
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
@@ -39,8 +34,8 @@
           modules = [./home-manager/.config/home-manager/shiva/home.nix];
         };
         # motherbase pc
-        "labellson@sulaco" = home-manager-25-05.lib.homeManagerConfiguration {
-          pkgs = nixpkgs-25-05.legacyPackages.x86_64-linux;
+        "labellson@sulaco" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
           modules = [./home-manager/.config/home-manager/sulaco/home.nix];
         };
       };
