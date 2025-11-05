7982c54a232560a0f7f3c6e2c3271aaf4cdc18aa
Daniel Laguna <labellson@fastmail.com>
2025-11-05 12:25:00 +0100

    darkman: add niri screen transition

--- darkman/.local/share/darkman/tinty.sh
+++ darkman/.local/share/darkman/tinty.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 ## tinty is almost all I need to apply colorschemes
 ## https://github.com/tinted-theming/tinty
+
+niri msg action do-screen-transition --delay-ms 250
 tinty apply "base16-selenized-${1}"
 
 # needed to tell active kitty windows to reload the theme
