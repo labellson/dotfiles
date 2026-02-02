a783f413ee26d6b58b1b33cfd35a1376163cb0da
Daniel Laguna Soto <daniel.lagunasoto@lely.com>
2026-02-02 11:09:15 +0100

    home(lelypop): add databricks shell abbreviations

--- home-manager/.config/home-manager/lelypop/fish.nix
+++ home-manager/.config/home-manager/lelypop/fish.nix
@@ -5,6 +5,15 @@
     shellAbbrs = {
       hms = "home-manager switch --flake ~/.dotfiles#labellson@lelypop";
       doom = "~/.config/emacs/bin/doom";
+
+      # databricks related
+      dbrlj = "databricks --profile dbr-sphere-dev jobs list -o json | jq '.[] | {job_id: .job_id, name: .settings.name}'";
+      dbrljtp = "databricks --profile dbr-sphere-dev jobs list -o json | jq '.[] | select(.settings.tags.tp_ticket == $tp_ticket) | {job_id: .job_id, name: .settings.name, tp_ticket: .settings.tags.tp_ticket}' --arg tp_ticket";
+      wdbrj = "watch -c -n 60 databricks --profile dbr-sphere-dev jobs list-runs --job-id";
+
+      dbrljprod = "databricks --profile dbr-sphere-prd jobs list -o json | jq '.[] | {job_id: .job_id, name: .settings.name}'";
+      dbrljtpprod = "databricks --profile dbr-sphere-prd jobs list -o json | jq '.[] | select(.settings.tags.tp_ticket == $tp_ticket) | {job_id: .job_id, name: .settings.name, tp_ticket: .settings.tags.tp_ticket}' --arg tp_ticket";
+      wdbrjprod = "watch -c -n 60 databricks --profile dbr-sphere-prd jobs list-runs --job-id";
     };
   };
 }
