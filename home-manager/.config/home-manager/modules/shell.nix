2b5857bd8cebe998e0c93a2a8d30fbe68f8cb1c3
Daniel Laguna <labellson@fastmail.com>
2026-05-30 21:22:03 +0200

    ref(fish): remove some old plugins

--- home-manager/.config/home-manager/modules/shell.nix
+++ home-manager/.config/home-manager/modules/shell.nix
@@ -77,18 +77,6 @@ in
           rev = "8ed76a62bb044ba4ad8e3e6832640178880df485";
           sha256 = "sha256-F1t81VliD+v6WEWqj1c1ehFBXzqLyumx5vV46s/FZRU=";
         })
-        (plugin {
-          owner = "evanlucas";
-          repo = "fish-kubectl-completions";
-          rev = "ced676392575d618d8b80b3895cdc3159be3f628";
-          sha256 = "sha256-OYiYTW+g71vD9NWOcX1i2/TaQfAg+c2dJZ5ohwWSDCc=";
-        })
-        (plugin {
-          owner = "rstacruz";
-          repo = "fish-asdf";
-          rev = "5869c1b1ecfba63f461abd8f98cb21faf337d004";
-          sha256 = "sha256-39L6UDslgIEymFsQY8klV/aluU971twRUymzRL17+6c=";
-        })
       ];
 
     shellAbbrs = {
