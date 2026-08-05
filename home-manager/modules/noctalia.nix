de6d699ed90bdb3163f2713ea985fdbe67b53308
Daniel Laguna Soto <daniel.lagunasoto@lely.com>
2026-08-05 15:09:05 +0200

    noctalia: enable screen-toolkit plugin

--- home-manager/modules/noctalia.nix
+++ home-manager/modules/noctalia.nix
@@ -26,10 +26,10 @@ in
     QT_QPA_PLATFORMTHEME = "gtk3";
   };
 
-  # TODO: packages needed by screen-toolkit plugin. plugin is for v4
   home.packages = with pkgs; [
-    grim slurp wl-clipboard imagemagick zbar curl
-    translate-shell wl-screenrec ffmpeg gifski
+    # packages needed by screen-toolkit plugin
+    slurp grim hyprpicker imagemagick zbar curl jq ffmpeg bc
+    wl-screenrec translate-shell satty
     (tesseract.override { enableLanguages = [ "eng" "spa" ]; })
 
     # packages needed by audio-switcher plugin
