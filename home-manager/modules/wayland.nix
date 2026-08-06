53fef318f75229e9eeca00306f6998464b363bfa
Daniel Laguna Soto <daniel.lagunasoto@lely.com>
2026-08-06 10:29:16 +0200

    wayland: add wl-mirror

--- home-manager/modules/wayland.nix
+++ home-manager/modules/wayland.nix
@@ -39,5 +39,7 @@ in
     # although is called this way, it provides pulseaudio-control to control
     # pulseaudio from any statusbar
     polybar-pulseaudio-control
+    # needed to mirror the screen
+    wl-mirror
   ];
 }
