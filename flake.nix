eb4b97cc351317f38389fbb0a07685965f8354fa
Daniel Laguna Soto <daniel.lagunasoto@lely.com>
2026-08-07 15:17:29 +0200

    nixpkgs: dirty change to unstable

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     nfsm-flake = {
       url = "github:gvolpe/nfsm";
@@ -92,7 +92,7 @@
           extraSpecialArgs = {
             inherit inputs pkgsUnstable dlsFuncs vars;
           };
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = nixpkgs-unstable.legacyPackages.x86_64-linux;
           modules = [
             ./home-manager/common
             ./hosts/common
